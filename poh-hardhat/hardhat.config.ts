fed3b4c94648ffa6d87806dd2b01827c281225b8
Juanu <juanu@juanu.com.ar>
2022-05-27 22:58:35 +0200

    Added etherscan section for verifying

--- poh-hardhat/hardhat.config.ts
+++ poh-hardhat/hardhat.config.ts
@@ -61,7 +61,9 @@ const config: HardhatUserConfig & { gasReporter: any, etherscan: any, settings: 
     timeout: 1200000
   },
   etherscan: {
-
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
 
